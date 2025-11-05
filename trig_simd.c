d75de5fa877ddbccfe184545185b107474e8493f
Matthis Ehrhardt <ehrhardt.matthis@gmail.com>
2025-11-05 13:49:35 +0100

    added laptop compilation

--- trig_simd.c
+++ trig_simd.c
@@ -332,3 +332,11 @@ void tan_simd(double *input, double *res, size_t n, int prec) {
 }
 
 // gcc ./cmeasure/cbind_to_hw_thread.c ./cmeasure/cmeasure.c ./cmeasure/CrystalClockInC.c ./trig_simd.c ./tests/test_interface_sin.c ./tests/value_generation.c ./tests/trig_arb_comparison.c -o test -lm -mavx -mavx2 -mfma -O2 -lflint -Wextra && ./test 1000000000 -8 8 1000000
+//
+// Compilation and run on laptop
+//
+//
+//gcc ./cmeasure/cbind_to_hw_thread.c ./cmeasure/cmeasure.c ./cmeasure/CrystalClockInC.c ./trig_simd.c ./tests/test_interface_sin.c ./tests/value_generation.c ./tests/trig_arb_comparison.c -o test -lm -mavx -mavx2 -mfma -O2 -Wextra $(pkg-config --cflags --libs flint)  && ./test 1000000 -8 8 100000
+
+
+//
