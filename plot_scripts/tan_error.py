c0f91fa81cb6b5260e3bd939cf8f3e812ca6bf72
matze-isses <ehrhardt.matthis@gmail.com>
2026-01-15 06:24:39 +0100

    made images of the problem
    Signed-off-by: matze-isses <ehrhardt.matthis@gmail.com>

--- plot_scripts/tan_error.py
+++ plot_scripts/tan_error.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def get_data(path="tan_error_behavior.tsv"):
+def get_data(path="/home/admin/tan_error_behavior_huge.tsv"):
     data = np.loadtxt(path, delimiter="\t", skiprows=1)
     x = data[:, 0]
     err = data[:, 1]
@@ -13,7 +13,7 @@ def simple_error_plot(x, err):
     plt.figure(figsize=(14, 8))
     plt.title("tan_simd absolute error vs input")
     plt.xlabel("x")
-    plt.ylabel("|tan(x) - tan_simd(x)|")
+    plt.ylabel("tan(x) - tan_simd(x)")
     plt.grid(True)
     plt.plot(x, err)
     plt.show()
@@ -30,16 +30,16 @@ def plot_range(x, err, bounds: tuple = (np.pi/2, 3/2 * np.pi)):
     plt.figure(figsize=(14, 8))
     plt.title("tan_simd absolute error vs input")
     plt.xlabel("x")
-    plt.ylabel("|tan(x) - tan_simd(x)|")
+    plt.ylabel("tan(x) - tan_simd(x)")
     plt.grid(True)
     plt.plot(plot_x, plot_y)
     plt.show()
 
 
 
-def problem_area(x, err):
+def problem_area_right(x, err):
     pi = np.pi
-    w = 1e-3   # window width
+    w = 1e-4   # window width
 
     xmin = x.min()
     xmax = x.max()
@@ -50,7 +50,7 @@ def problem_area(x, err):
     plt.figure(figsize=(14, 8))
     plt.title(r"tan_simd absolute error near poles $(1+2n)\pi/2$ (centered overlays)")
     plt.xlabel(r"offset from pole $x - (1+2n)\pi/2$")
-    plt.ylabel(r"$|tan(x) - tan\_simd(x)|$")
+    plt.ylabel(r"$tan(x) - tan\_simd(x)$")
     plt.grid(True)
 
     count = 0
@@ -80,7 +80,51 @@ def problem_area(x, err):
     plt.show()
 
 
+def problem_area_left(x, err):
+    pi = np.pi
+    w = 1e-5   # window width: pole - 0.0001 .. pole
+
+    xmin = x.min()
+    xmax = x.max()
+
+    # Polstellen: (1 + 2n) * pi/2
+    n_min = int(np.floor((2 * xmin / pi - 1) / 2))
+    n_max = int(np.ceil((2 * xmax / pi - 1) / 2))
+
+    plt.figure(figsize=(14, 8))
+    plt.title(r"tan_simd absolute error left of poles $(1+2n)\pi/2$ (centered overlays)")
+    plt.xlabel(r"offset from pole $x - (1+2n)\pi/2$")
+    plt.ylabel(r"$tan(x) - tan\_simd(x)$")
+    plt.grid(True)
+
+    count = 0
+
+    for n in range(n_min, n_max + 1):
+        pole = (1 + 2*n) * pi / 2.0
+
+        # window [pole - w, pole]
+        mask = (x >= pole - w) & (x <= pole)
+        if not np.any(mask):
+            continue
+
+        xw = x[mask]
+        ew = err[mask]
+
+        # Zentrieren: jetzt liegt der Bereich in [-w, 0]
+        t = xw - pole
+
+        label = rf"$x = \frac{{(1+2\cdot{n})\pi}}{{2}}$"
+        plt.plot(t, ew, alpha=0.7, label=label)
+        count += 1
+
+    plt.xlim(-w, 0.0)
+    plt.legend(loc="upper left", fontsize="small", ncol=2)
+    print(f"Plotted {count} pole windows (left side).")
+    plt.show()
+
+
 if __name__ == "__main__":
     x, err = get_data()
-    plot_range(x, err)
-    # problem_area(x, err)
+    # plot_range(x, err)
+    # problem_area_right(x, err)
+    problem_area_left(x, err)
