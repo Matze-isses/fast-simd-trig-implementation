23386f17816967b5f8fd2e4f1ed0e5d65dfa6e0c
matze-isses <ehrhardt.matthis@gmail.com>
2026-02-09 06:02:32 +0100

    changes for AVX512
    Signed-off-by: matze-isses <ehrhardt.matthis@gmail.com>

--- trig_simd.h
+++ trig_simd.h
@@ -4,7 +4,6 @@
 #pragma once
 #include <immintrin.h>
 
-#define __AVX2__ 1
 #define M_PI_8 (M_PI / 8)
 #define TAN_CORRECTION (0.00000000000000006123233995736765)
 #define RANG_REDUCTION_CORRECTION (3.8981718325193755e-17)
@@ -57,13 +56,17 @@
 #endif // __AVX2__ 
 
 #if defined(__AVX512__) && defined(__AVX512F__)
+
 #define SIMD_LENGTH (512)
+#define SIMD_DOUBLES (8)
 
 // Double I/O
 #define SDOUBLE __m512d
-#define LOAD_DOUBLE         _mm512_set1_pd
-#define LOAD_DOUBLE_VEC     _mm512_loadu_pd
-#define SIMD_TO_DOUBLE_VEC  _mm512_storeu_pd
+#define LOAD_DOUBLE _mm512_set1_pd
+#define LOAD_DOUBLE_VEC _mm512_loadu_pd
+#define SIMD_TO_DOUBLE_VEC _mm512_storeu_pd
+
+#define SET_ZERO _mm512_setzero_pd
 
 // Double Operations
 #define MUL_DOUBLE_S _mm512_mul_pd
@@ -76,14 +79,19 @@
 #define FLOOR_DOUBLE_S(a) \
     _mm512_roundscale_pd((a), _MM_FROUND_TO_NEG_INF | _MM_FROUND_NO_EXC)
 
-// for (a, b, c) returns (a * b) + c
+// for (a, b, c) does return (a * b) + c
 #define FMADD_PD _mm512_fmadd_pd
 
 // ceil(a)
 #define CEIL_PD(a) \
     _mm512_roundscale_pd((a), _MM_FROUND_TO_POS_INF | _MM_FROUND_NO_EXC)
 
-// |a|
+// keep the same macro names as AVX2
+#define CMP_PD _mm512_cmp_pd_mask
+
+// AVX512 blend-by-mask equivalent (mask in k-register)
+#define BLEND_PD(src, a, mask) _mm512_mask_blend_pd((mask), (src), (a))
+
 #define ABS_PD(a) \
     _mm512_max_pd((a), _mm512_mul_pd(_mm512_set1_pd(-1.0), (a)))
 
@@ -92,14 +100,14 @@
     _mm512_mask_mov_pd( \
         (a), \
         _mm512_cmp_pd_mask( \
-            _mm512_andnot_pd(_mm512_set1_pd(-0.0), (a)), /* clear sign bit */ \
+            _mm512_andnot_pd(_mm512_set1_pd(-0.0), (a)), \
             _mm512_set1_pd(INFINITY), \
             _CMP_EQ_OQ \
         ), \
         _mm512_setzero_pd() \
     )
 
-#endif
+#endif // __AVX512__ && __AVX512F__
 
 void sin_simd(double *input, double *res, size_t n);
 void tan_simd(double *input, double *res, size_t n);
